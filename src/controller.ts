d58babf4c00f9be4451870519866caa09843a9e9
Guilherme Beidaki Peres <guibperes@gmail.com>
2020-05-06 13:06:08 -0300

    renamed app files

--- src/controller.ts
+++ src/controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Inject } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from './service';
 
 @Controller()
 export class AppController {
