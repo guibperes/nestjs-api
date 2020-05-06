d1da7c640c25f9d2a5f53eaa1c5478e868dd8fe6
Guilherme Beidaki Peres <guibperes@gmail.com>
2020-05-06 15:34:53 -0300

    added body validation

--- src/api/book/BookDTO.ts
+++ src/api/book/BookDTO.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsNumber, IsOptional, Length, Min } from 'class-validator';
+
 export class BookDTO {
+  @IsNotEmpty()
+  @Length(4, 40)
   title: string;
-  description?: string;
+
+  @IsOptional()
+  @Length(4, 255)
+  description: string;
+
+  @IsNumber()
+  @Min(1)
   pages: number;
 }
